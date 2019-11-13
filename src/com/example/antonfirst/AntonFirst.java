218427add626dcced54c05eec0ad02ba7f46a05c
AntonKam <kaminskyianton@gmail.com>
2019-11-13 20:03:53 +0200

    Init commit

---
+++ src/com/example/antonfirst/AntonFirst.java
@@ -0,0 +1,33 @@
+package com.example.antonfirst;{
+
+
+public class AntonFirst {
+
+    int i = 0;
+        while (i < 10);
+    {
+        i++;
+        System.out.println(i);
+    }
+}
+}
+
+
+
+
+
+//    public static void main(String[] args) {
+//
+//        Person person = new Person();
+//        person.name = "John";
+//
+//        public void whoAmI () {
+//            System.out.println(person.name);
+//
+//
+//        }
+//
+//    }
+//}
+
+
