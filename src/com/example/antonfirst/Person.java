dac422957f0a64f1c572d52eded5c31855f4c44d
AntonKam <kaminskyianton@gmail.com>
2019-11-13 20:17:48 +0200

    sjdfjsdjf

--- src/com/example/antonfirst/Person.java
+++ src/com/example/antonfirst/Person.java
@@ -1,25 +1,25 @@
-package com.example.antonfirst.;
-
-public class Person {
-
-    String name;
-    int age;
-    int height
-
-
-    public Person () {
-    }
-
-    public Person (String name){
-        this.name = name;
-        this.age = 12;
-    }
-
-    public int getAge (){
-        return age;
-    }
-
-    public Person getPerson(){
-        return this;
-    }
-}
+//package com.example.antonfirst.;
+//
+//public class Person {
+//
+//    String name;
+//    int age;
+//    int height
+//
+//
+//    public Person () {
+//    }
+//
+//    public Person (String name){
+//        this.name = name;
+//        this.age = 12;
+//    }
+//
+//    public int getAge (){
+//        return age;
+//    }
+//
+//    public Person getPerson(){
+//        return this;
+//    }
+//}
